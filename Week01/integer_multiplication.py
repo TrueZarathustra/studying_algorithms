daa0a34e3b79c6547c18cb954cb16d426657dcd0
vvorotnikov <v.s.vorotnikov@gmail.com>
2014-05-06 17:14:29 +0400

    added Karatsuba algorithm

--- Week01/integer_multiplication.py
+++ Week01/integer_multiplication.py
@@ -25,3 +25,45 @@ a = random.randint(2**2048,2**2049)
 b = random.randint(2**2048,2**2049)
 print "Random multiplication: " + str(schoolMultiplication(a,b))
 '''
+
+def KaratsubaMultiplication(number1,number2):
+
+    if number2 > number1:
+        temp = number2
+        number2 = number1
+        number1 = temp
+    
+    n = len(str(number1))
+    if n>len(str(number2)):
+        zeros = ''
+        for i in range(n-len(str(number2))):
+            zeros+='0'
+        str2 = zeros + str(number2)
+        str1 = str(number1)
+    else:
+        str1 = str(number1)
+        str2 = str(number2)
+
+    if  n == 1:
+        return number1*number2
+
+    a = int(str1[0:(n+n%2)//2])    
+    b = int(str1[(n+n%2)//2:]) 
+    c = int(str2[0:(n+n%2)//2])    
+    d = int(str2[(n+n%2)//2:])
+    
+    x = KaratsubaMultiplication(a,c)
+    y = KaratsubaMultiplication(b,d)
+    z = KaratsubaMultiplication((a+b),(c+d)) - x - y
+
+    return x*(10**(n-n%2)) + z*(10**(n//2)) + y
+
+'''
+#easy test
+for i in range(1,999):
+    for j in range(1,999):
+        if i*j != KaratsubaMultiplication(i,j):
+            print "Alert! Some problems with ", i, j
+        elif i%100==0 and j%100==0:
+            print i,j #just to see progress
+'''
